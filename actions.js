d8f0f6f912b1555b0db03cdebfe9b0589a1ace26
elena-martin <elenamartindesign@gmail.com>
2022-06-04 22:32:15 -0700

    commit

--- actions.js
+++ actions.js
@@ -1,10 +1,14 @@
-const { Octokit } = require("@octokit/core");
-const octokit = new Octokit({
-    auth: 'personal-access-token123'
-  })
-  
-  octokit.request('GET /repos/elena-martin/qimo-bot/actions/jobs/{job_id}/logs', {
-    owner: 'OWNER',
-    repo: 'REPO',
-    job_id: 'JOB_ID'
-  })
+const { data } = require('jquery');
+const fetch = require('./node_modules/node-fetch');
+
+var rqst = 'https://api.github.com/repos/elena-martin/qimo-bot/actions/workflows/27516000/runs';
+function getStatus(){
+    const response = fetch(rqst)
+    .then(response => response.json())
+    .then(data => {
+        let run = data.workflow_runs[0].status;
+        console.log(run);
+    });
+};
+getStatus();
+
